94a1ddcdf43279e17e35216de57c5eae2e84aa9a
Ashish Bhattarai <ashishbhattarai@protonmail.com>
2019-05-09 19:12:49 +0545

    Fixed ammojs usage.

--- src/physics/physics_world.ts
+++ src/physics/physics_world.ts
@@ -1,19 +1,18 @@
-import {Ammo} from 'ammo.js';
-import {vec3, mat4} from 'gl-matrix';
+import "@stardazed/ammo";
 class PhysicsWorld {
-    gravity:vec3 =  vec3.create();
-    constructor(){   
-    let broadphase =  new Ammo.btDbvtBroadphase();
+  constructor() {
+    let broadphase = new Ammo.btDbvtBroadphase();
     let collConfig = new Ammo.btDefaultCollisionConfiguration();
-	let dispatcher = new Ammo.btCollisionDispatcher(collConfig);
+    let dispatcher = new Ammo.btCollisionDispatcher(collConfig);
     let solver = new Ammo.btSequentialImpulseConstraintSolver();
-    
-    let dynamicsWorld = new Ammo.btDiscreteDynamicsWorld(dispatcher, broadphase,solver,collConfig);
-    
-    dynamicsWorld.setGravity(this.gravity[0] = 0.0,this.gravity[1] = -10.0, this.gravity[2] = 0.0 );
 
-    }
+    let dynamicsWorld = new Ammo.btDiscreteDynamicsWorld(
+      dispatcher,
+      broadphase,
+      solver,
+      collConfig
+    );
 
-    
-
+    dynamicsWorld.setGravity(new Ammo.btVector3(0.0, -10.0, 0.0));
+  }
 }
