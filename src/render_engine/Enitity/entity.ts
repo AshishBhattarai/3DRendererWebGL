5966dc4a0629630efdb10e5ca533da83aa2a6131
Ashish Bhattarai <ashishbhattarai@protonmail.com>
2019-05-09 19:33:40 +0545

    Updated entity and light class.

--- src/render_engine/Enitity/entity.ts
+++ src/render_engine/Enitity/entity.ts
@@ -1,17 +1,22 @@
 import { vec3, mat4 } from "gl-matrix";
-import Light from "./light";
 
 export default class Enitity {
   public position: vec3;
   public rotation: vec3;
   public scale: vec3;
+  public modelName: string;
   private transformation: mat4;
-  
 
-  constructor() {
-    this.position = vec3.create();
-    this.rotation = vec3.create();
-    this.scale = vec3.create();
+  constructor(
+    modelName: string,
+    position = vec3.fromValues(0, 0, 0),
+    rotation = vec3.fromValues(0, 0, 0),
+    scale = vec3.fromValues(1, 1, 1)
+  ) {
+    this.modelName = modelName;
+    this.position = position;
+    this.rotation = rotation;
+    this.scale = scale;
 
     this.transformation = mat4.create();
     mat4.identity(this.transformation);
@@ -19,13 +24,10 @@ export default class Enitity {
 
   public getTransMatrix() {
     mat4.translate(this.transformation, this.transformation, this.position);
-    mat4.rotateY(this.transformation, this.transformation, this.rotation[0]);
-    mat4.rotateX(this.transformation, this.transformation, this.rotation[1]);
+    mat4.rotateY(this.transformation, this.transformation, this.rotation[1]);
+    mat4.rotateX(this.transformation, this.transformation, this.rotation[0]);
     mat4.rotateZ(this.transformation, this.transformation, this.rotation[2]);
     mat4.scale(this.transformation, this.transformation, this.scale);
     return this.transformation;
   }
-
-
-
 }
--- src/render_engine/Enitity/light.ts
+++ src/render_engine/Enitity/light.ts
@@ -1,15 +1,32 @@
 import { vec3, mat4, vec4 } from "gl-matrix";
-import Enitity from "./entity";
-class Light {
- public position:vec3;
- private rotation:vec3;
- public color:vec3; 
 
-  constructor() {
-    this.position = vec3.create();
-    this.rotation = vec3.create();
-    this.color = vec3.create();
+export default class Light {
+  public position: vec4;
+  public color: vec3;
+
+  private rotation: vec3;
+  private rotMat: mat4;
+
+  constructor(
+    color = vec3.fromValues(0, 0, 0),
+    position = vec4.fromValues(0, 0, 0, 0),
+    rotation = vec3.fromValues(0, 0, 0)
+  ) {
+    this.color = color;
+    this.position = position;
+    this.rotMat = mat4.create();
+    mat4.identity(this.rotMat);
+
+    this.rotate(rotation);
+  }
+
+  public rotate(rotation: vec3) {
+    this.rotation = rotation;
+
+    mat4.rotateY(this.rotMat, this.rotMat, rotation[1]);
+    mat4.rotateX(this.rotMat, this.rotMat, rotation[0]);
+    mat4.rotateZ(this.rotMat, this.rotMat, rotation[2]);
+
+    vec4.transformMat4(this.position, this.position, this.rotMat);
   }
 }
-
-export default Light;
