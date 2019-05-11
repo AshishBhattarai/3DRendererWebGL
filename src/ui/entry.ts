da33b3adbf2899b32c7f396257023b0b88114b90
Aashan Ghimire <ashanghimire10@gmail.com>
2019-05-12 03:15:34 +0545

    UI color improvement

--- src/ui/entry.ts
+++ src/ui/entry.ts
@@ -7,7 +7,27 @@ export function renderUI(){
 
     let box2 = new Box("Worlds");
     box2.push(new Button("Earth", function() {}).render());
-    box2.push(new Button("Space", function() {}).render());
+    box2.push(new Button("Space", function() {
+        let loader = new Box('Comming Soon');
+        let info = document.createElement('h3');
+        info.style.margin = '0';
+        info.style.padding = '0';
+        info.style.fontWeight = '200';
+        info.innerHTML = 'This feature will be available in the near future!!';
+        loader.push(info);
+        document.getElementsByTagName("body")[0].appendChild(
+            loader.render({
+                top: "200px",
+                left: "40%",
+                width: '220px'
+            })
+        );
+        setTimeout(() => {
+            document
+                .getElementsByTagName("body")[0]
+                .removeChild(loader.render());
+        }, 5000);
+    }).render());
     document.getElementsByTagName("body")[0].appendChild(
       box1.render({
         position: "absolute",
